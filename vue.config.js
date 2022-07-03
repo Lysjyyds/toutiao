325a17c2f5c0dca963e42afeffa1fb32f6a24b8f
zjh <zjiahe0124@163.com>
2022-07-03 11:14:21 +0800

    提交工作

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+const { join } = require('path')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    devServer: {
+      open: true,
+      port: 3000,
+      host: 'localhost'
+    },
+    resolve: {
+      alias: {
+        '#': join(__dirname, 'src/components')
+      }
+    }
+  }
 })
